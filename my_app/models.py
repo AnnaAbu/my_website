119bdcd4c403f7e008b74b4b2e3bf1a242b9a867
liuzhitian <15242008@bjtu.edu.cn>
2017-11-11 14:49:33 +0800

    1.1

--- my_app/models.py
+++ my_app/models.py
@@ -6,21 +6,17 @@ from django.db import models
 # Create your models here.
 class Article(models.Model):
 	Category_Choice=(
-		('1','实验室简介'),
-		('2','新闻动态'),
-		('3','通知公告'),
-	)
-	student_category=(
-		('1','本科生')
-		('2','研究生')
-		('3','博士生')
+		('kydt','科研动态'),
+		('yjcg','研究成果'),
+		('bks','本科生'),
+		('yjs','研究生'),
+		('bss','博士生'),
 	)
 	id = models.AutoField(primary_key=True)
 	title = models.CharField('标题',max_length=255, blank=True)
 	content = models.TextField('文章内容')
 	timestamp = models.CharField('时间',max_length=20, blank=True)
-	category=models.CharField('文章分类',max_length=1,choices=Category_Choice)
-	student_category=models.CharField('学生分类',max_length=1,choices=student_category)
+	category=models.CharField('文章分类',max_length=20,choices=Category_Choice)
 	class Meta:
 		db_table='article'
 class Picture(models.Model):
--- my_app/views.py
+++ my_app/views.py
@@ -7,38 +7,34 @@ import string
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 # Create your views here.
-def queryset_to_dictlist(mylist,name):
+def queryset_to_dictlist(mylist,attrlist):
 	list=[]
-	count = 0
+	count=0
 	for l in mylist:
 		mydict = {}
-		mydict['id'] = l[0]
-		mydict['student_category'] = l[1]
-		mydict['title'] = l[2]
+		for k in attrlist:
+			mydict[k]=l[count]
+			count+=1
 		list.append(mydict)
 	return list
 
 
 def homepage(request):
-	if request.method == 'GET':
-		return JsonResponse({'status': '1', 'msg': 'invalid type'})
-	elif request.method == 'POST':
-		mylist_xwdt = Article.objects.filter(category='2').order_by('-id').values_list('id', 'student_category', 'title')[0:6]
-		mylist_tzgg = Article.objects.filter(category='1').order_by('-id').values_list('id', 'student_category', 'title')[0:6]
+	#if request.method == 'GET':
+		#return JsonResponse({'status': '1', 'msg': 'invalid type'})
+	#elif request.method == 'POST':
+		#mylist_xwdt = Article.objects.filter(category='2').order_by('-id').values_list('id', 'student_category', 'title')[0:6]
+		#mylist_tzgg = Article.objects.filter(category='1').order_by('-id').values_list('id', 'student_category', 'title')[0:6]
 		mylist_pic = Picture.objects.all().order_by('-id')[0:3]
 		listdict = {}
-		listdict['xwdt'] = queryset_to_dictlist(mylist_xwdt, 'xwdt')
-		listdict['tzgg'] = queryset_to_dictlist(mylist_xwdt, 'tzgg')
-		picdict = {}
-		count = 0
+		##listdict['tzgg'] = queryset_to_dictlist(mylist_xwdt, 'tzgg')
+		piclist=[]
 		for t in mylist_pic:
 			mydict = {}
 			mydict['pic_url'] = '/site_media/' + str(t.image)
 			mydict['news_url'] = t.url
-			count += 1
-			dictname = 'pic_'  + str(count)
-			picdict[dictname] = mydict
-		listdict['picture'] = picdict
+			piclist.append(mydict)
+		listdict['picture'] = piclist
 		#listdict['introduction'] = '一个静态页面，url前端给'
 		#状态码
 		listdict['status'] = '0'
@@ -48,45 +44,66 @@ def homepage(request):
 
 def getlist(request):
 	if request.method == 'GET':
-		return JsonResponse({'status': '1', 'msg': 'invalid type'})
+		getpager= request.GET.get('page',1)
+		getcategory=request.GET.get('category')
+		getnum=request.GET.get('num',3)
+		#return JsonResponse({'status': '1', 'msg': 'invalid type'})
 	elif request.method == 'POST':
 		getpager = request.POST.get('page', 1)		
 		getcategory = request.POST.get('category')
-		getcategory=getcategory.strip()
-		if getcategory==None:
-			return JsonResponse({'status': '1', 'msg': 'not found category'})
-		page_articlenum = 15
-		try:
-			getpager=int(getpager)
-		except ValueError:
-			return JsonResponse({'status': '1', 'msg': 'page is not an Integer'})
-		if (getpager - 1) * page_articlenum > Article.objects.count():
-			return JsonResponse({'status': '1', 'msg': 'invalid page'})
-		elif getpager * page_articlenum < Article.objects.count():
-			if getcategory=='all':
-				mylist = Article.objects.all().order_by('-id').values_list('id','title','timestamp')[(getpager-1)*page_articlenum:page_articlenum]
-			else:
-				mylist = Article.objects.filter(category=getcategory).order_by('-id').values_list('id', 'title', 'timestamp')[(getpager-1)*page_articlenum:page_articlenum]
+		getnum=request.POST.get('num',3)
+	#getcategory=getcategory.strip()
+	if getcategory==None:
+		return JsonResponse({'status': '1', 'msg': 'not found category'})
+	if isinstance(getcategory,[]):
+		return JsonResponse({'status':'1','msg':'invalid type'})
+	#page_articlenum = 15
+	try:
+		getpager=int(getpager)
+		getnum=int(getnum)
+	except ValueError:
+		return JsonResponse({'status': '1', 'msg': 'page or num is not an Integer'})
+
+	mydict={}
+	for l in getcategory:
+		backlist=[]
+		if (getpager - 1) * getnum > Article.objects.filter(category=l).count():
+			return JsonResponse({'status': '1', 'msg': 'invalid page or num'})
+		elif getpager*getnum<Article.objects.filter(category=l).count():
+			mylist=Article.objects.filter(category=l).values_list('category','id','title','timestamp')[(getpager-1)*getnum:getnum]
 		else:
-			if getcategory=='all':
-				mylist = Article.objects.all().order_by('-id').values_list('id', 'title', 'timestamp')[(getpager - 1) * page_articlenum:Article.objects.count()-(getpager-1)*page_articlenum]
-			else:
-				mylist = Article.objects.filter(category=getcategory).order_by('-id').values_list('id', 'title','timestamp')[(getpager - 1) * page_articlenum:Article.objects.count() - (getpager - 1) * page_articlenum]
+			mylist=Article.objects.filter(category=l).values_list('category','id','title','timestamp')[
+				   (getpager-1)*getnum:Article.objects.filter(category=l).count()-(getpager-1)*getnum]
+		mydict[l]=queryset_to_dictlist(mylist,['category','id','title','timestamp'])
 
-		mydict={}
+	#elif getpager * getnum < Article.objects.count():
 
-		list=[]
-		for l in mylist:
-			dict = {}
-			dict['id'] = l[0]
-			dict['title'] = l[1]
-			dict['timestamp'] = l[2]
-			list.append(dict)
-		if list==[]:
-			return JsonResponse({'status': '1', 'msg': 'invalid category'})
-		mydict['status'] = '0'
-		mydict['data']=list
-		return JsonResponse(mydict)
+		'''
+		if getcategory=='all':
+			mylist = Article.objects.all().order_by('-id').values_list('id','title','timestamp')[(getpager-1)*page_articlenum:page_articlenum]
+		else:
+			mylist = Article.objects.filter(category=getcategory).order_by('-id').values_list('id', 'title', 'timestamp')[(getpager-1)*page_articlenum:page_articlenum]
+	else:
+		if getcategory=='all':
+			mylist = Article.objects.all().order_by('-id').values_list('id', 'title', 'timestamp')[(getpager - 1) * page_articlenum:Article.objects.count()-(getpager-1)*page_articlenum]
+		else:
+			mylist = Article.objects.filter(category=getcategory).order_by('-id').values_list('id', 'title','timestamp')[(getpager - 1) * page_articlenum:Article.objects.count() - (getpager - 1) * page_articlenum]
+		
+	mydict={}
+
+	list=[]
+	for l in mylist:
+		dict = {}
+		dict['id'] = l[0]
+		dict['title'] = l[1]
+		dict['timestamp'] = l[2]
+		list.append(dict)
+	if list==[]:
+		return JsonResponse({'status': '1', 'msg': 'invalid category'})
+		'''
+	mydict['status'] = '0'
+	#mydict['data']=list
+	return JsonResponse(mydict)
 		# elif (page+1)*page_articlenum-Article.objects.count<page_articlenum:
         # list里面就是[page*page_articlenum:Article.object.count-page*page_articlenum]
         # pass
@@ -96,7 +113,8 @@ def getlist(request):
 
 def detail(request):
 	if request.method == 'GET':
-		return JsonResponse({'status':'1','msg':'invalid type'})
+		getid=request.GET.get('id')
+		#return JsonResponse({'status':'1','msg':'invalid type'})
 	elif request.method == 'POST':
 		getid = request.POST['id']
 	try:
